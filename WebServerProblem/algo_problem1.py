3c1321784355e6a244bdfedebf38bd50b8a7dcd2
Pushpak Maheshwari <122460779+PushpakMaheshwari1@users.noreply.github.com>
2023-03-12 10:46:35 +0530

    Update algo_problem1.py

--- WebServerProblem/algo_problem1.py
+++ WebServerProblem/algo_problem1.py
@@ -1,9 +1,10 @@
 def server(lists):
+  ## Using brute force approach
   for i in range(len(lists)-1):  
     for j in range(i+1,len(lists)-1):
         if lists[i][0] > lists[j][0]:
             lists[i],lists[j] = lists[j],lists[i]
 
         elif lists[i][0] == lists[j][0] and lists[j][1] > lists[i][1]:
-            lists[i],lists[j] = lists[j],lists[i]
+            lists[i],lists[j] = lists[j],lists[i]  
   return lists
