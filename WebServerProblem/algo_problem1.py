78a3836795d0dc6057562c3d2df429b2ba2ecdcf
Pushpak Maheshwari <122460779+PushpakMaheshwari1@users.noreply.github.com>
2023-03-11 21:58:29 -0800

    Update algo_problem1.py

--- WebServerProblem/algo_problem1.py
+++ WebServerProblem/algo_problem1.py
@@ -1,5 +1,5 @@
+## Using brute force approach
 def server(lists):
-  ## Using brute force approach
   for i in range(len(lists)-1):  
     for j in range(i+1,len(lists)-1):
         if lists[i][0] > lists[j][0]:
@@ -8,3 +8,8 @@ def server(lists):
         elif lists[i][0] == lists[j][0] and lists[j][1] > lists[i][1]:
             lists[i],lists[j] = lists[j],lists[i]  
   return lists
+
+## Using python based approach
+def website(lists):
+  sorted_list = sorted(lists,lambda : x[0],-x[1])
+  return(sorted_lists)
