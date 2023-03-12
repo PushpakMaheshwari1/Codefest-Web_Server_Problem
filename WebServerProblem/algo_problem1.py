88d6f303516c0023b52186916b52eefedfbe82be
Pushpak Maheshwari <122460779+PushpakMaheshwari1@users.noreply.github.com>
2023-03-12 10:44:01 +0530

    Create algo_problem1.py

---
+++ WebServerProblem/algo_problem1.py
@@ -0,0 +1,9 @@
+def server(lists):
+  for i in range(len(lists)-1):  
+    for j in range(i+1,len(lists)-1):
+        if lists[i][0] > lists[j][0]:
+            lists[i],lists[j] = lists[j],lists[i]
+
+        elif lists[i][0] == lists[j][0] and lists[j][1] > lists[i][1]:
+            lists[i],lists[j] = lists[j],lists[i]
+  return lists
